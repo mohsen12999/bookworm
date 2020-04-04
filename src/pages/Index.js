6cf21bf92e1bfe9feabdbf53ee24c8669a3bed27
mohsen shabanian - work <mohsen1299@gmail.com>
2020-04-05 02:01:15 +0430

    add search span in index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+import "./index.css";
 
 const useStyles = makeStyles(theme => ({
   // root: {
@@ -43,25 +48,59 @@ const useStyles = makeStyles(theme => ({
   //     fontSize: "2rem"
   //   }
   // }
-  bidSlider: {
-    background: "lightgray",
-    display: "display",
-    [theme.breakpoints.up("sm")]: {
-      display: "flex"
-    }
+  // bidSlider: {
+  //   background: "lightgray",
+  //   display: "display",
+  //   [theme.breakpoints.up("sm")]: {
+  //     display: "flex"
+  //   }
+  // },
+  // image: {
+  //   width: "100%",
+  //   [theme.breakpoints.up("sm")]: {
+  //     width: "50%"
+  //   }
+  // },
+  // imageSpan: {
+  //   flexGrow: "1",
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   direction: "rtl"
+  // }
+  // search_div: {
+  //   background: "lightgray",
+  //   textAlign: "center",
+  //   paddingTop: "2em",
+  //   paddingBottom: "2em",
+  //   direction: "rtl"
+  // },
+  // searchText: {
+  //   //display: "block"
+  //   width: "80%",
+  //   [theme.breakpoints.up("sm")]: {
+  //     width: "50%"
+  //   }
+  // },
+  // searchBtn: {
+  //   marginTop: "2rem"
+  // },
+  mainBanner: {
+    // marginTop: "10px",
+    // marginRight: "-14px",
+    // marginLeft: "-14px",
+    // background: "white"
   },
-  image: {
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "50%"
-    }
+  searchText: {
+    width: "98%"
   },
-  imageSpan: {
-    flexGrow: "1",
+  grid_item: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
-    direction: "rtl"
+    alignItems: "center"
+  },
+  imgSlider: {
+    width: "100%"
   }
 }));
 
@@ -70,19 +109,41 @@ const Index = () => {
 
   return (
     <React.Fragment>
-      <div className={classes.bidSlider}>
+      <div className={classes.mainBanner}>
         <img
-          src="/img/slider/slider-book.png"
-          className={classes.image}
-          alt=""
+          src="img/slider/slider1-skybook1-1368x599.jpg"
+          className={classes.imgSlider}
+          alt="img"
         />
-        <div className={classes.imageSpan}>
-          <div>
-            <Typography variant="h3">عنوان کتاب</Typography>
-            <Typography paragraph>کتاب جدید نویسنده محبوب</Typography>
-          </div>
-        </div>
       </div>
+      <form className="search-grid persian-form">
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={3} className={classes.grid_item}>
+            <Typography variant="h5" component="h2">
+              جستجوی کتاب
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} className={classes.grid_item}>
+            <TextField
+              className={classes.searchText}
+              id="standard-basic"
+              variant="outlined"
+              label="جستجوی عنوان کتاب، نویسنده یا موضوع"
+            />
+          </Grid>
+          <Grid item xs={12} sm={2} className={classes.grid_item}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.searchBtn}
+              size="large"
+              type="submit"
+            >
+              جستجو
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
 
       <Typography paragraph style={{ marginTop: "2em" }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -98,6 +159,7 @@ const Index = () => {
         varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
         Lorem donec massa sapien faucibus et molestie ac.
       </Typography>
+
       <Typography paragraph>
         Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
         ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
