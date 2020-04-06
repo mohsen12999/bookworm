6babef085ff41047e1674f4fb4368b4b9d094ab5
mohsen shabanian - work <mohsen1299@gmail.com>
2020-04-06 18:53:18 +0430

    add link to book thumbnail

--- src/components/BookThumbnail.js
+++ src/components/BookThumbnail.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./BookThumbnail.css";
 // TODO: new book banner
 // TODO: sale banner
 
 const BookThumbnail = props => (
-  <div className="book-thumb">
+  <Link to={"/book/" + props.id} className="book-thumb">
     <img src={props.img} alt={props.title + " - " + props.author} />
     <p>{props.title}</p>
     <p>{props.author}</p>
     <p className="persian-number">{props.price} هزار تومان</p>
-  </div>
+  </Link>
 );
 
 export default BookThumbnail;
