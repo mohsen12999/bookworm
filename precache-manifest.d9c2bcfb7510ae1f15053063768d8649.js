2082c6d794f50c1b1f76c228a0789ed747b0a041
mohsen shabanian - work <mohsen1299@gmail.com>
2020-04-08 02:50:40 +0430

    Updates

---
+++ precache-manifest.d9c2bcfb7510ae1f15053063768d8649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be3b5e1037e7075bb167a757383d9b2",
+    "url": "/bookworm/index.html"
+  },
+  {
+    "revision": "d4006a79bfe1ef2d2f90",
+    "url": "/bookworm/static/css/main.46ed47f6.chunk.css"
+  },
+  {
+    "revision": "a05481adba58dec952ce",
+    "url": "/bookworm/static/js/2.8c170fea.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/bookworm/static/js/2.8c170fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4006a79bfe1ef2d2f90",
+    "url": "/bookworm/static/js/main.2da9c205.chunk.js"
+  },
+  {
+    "revision": "097bdb66458ff04fd875",
+    "url": "/bookworm/static/js/runtime-main.0b6cd3c3.js"
+  }
+]);
