efefb24fd81d4e8313879aded635feec8826bdda
mohsen shabanian - work <mohsen1299@gmail.com>
2020-04-10 01:18:12 +0430

    make mybook in admin

--- src/pages/admin/MyBook.js
+++ src/pages/admin/MyBook.js
@@ -1,5 +1,42 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
-const MyBook = () => <div>My Book</div>;
+import { AuthContext } from "../../contexts/AuthContext";
+import BookThumbnail from "../../components/BookThumbnail";
+
+import { books } from "../../services/data";
+
+import "../BookList.css";
+
+const MyBook = () => (
+  <AuthContext.Consumer>
+    {(context) => {
+      const myBook = books.filter((book) =>
+        context.boughtBook.includes(book.id)
+      );
+      return (
+        <div>
+          <Typography variant="h5" component="h3" className="section-title">
+            کتاب های شما
+          </Typography>
+          <Grid container spacing={1}>
+            {myBook.map((book) => (
+              <Grid key={book.id} item xs={6} sm={3}>
+                <BookThumbnail
+                  id={book.id}
+                  title={book.title}
+                  img={book.img}
+                  author={book.author}
+                  price={book.price}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      );
+    }}
+  </AuthContext.Consumer>
+);
 
 export default MyBook;
