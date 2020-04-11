f89496869cf05ee95b65ff07948053c74d402120
mohsen shabanian - work <mohsen1299@gmail.com>
2020-04-12 01:03:05 +0430

    reading book chapter and can going top

--- src/pages/ReadBook.js
+++ src/pages/ReadBook.js
@@ -1,8 +1,109 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import { useParams, Link } from "react-router-dom";
+
+import ScrollTop from "../components/ScrollTop";
+import { getSingleChapter } from "../services/data";
+
+import "./ReadBook.css";
 
 // TODO: prev and next chapter
 // TODO: back to book page
 
-const ReadBook = () => <div>My Note</div>;
+const ReadBook = (props) => {
+  const { book_id, chapter_id } = useParams();
+  const {
+    chapter,
+    prev_id,
+    prev_title,
+    next_id,
+    next_title,
+  } = getSingleChapter(book_id, chapter_id);
+
+  return (
+    <div className="chapter-page">
+      {prev_id && (
+        <Button
+          variant="outlined"
+          size="large"
+          color="primary"
+          className="chapter-change"
+          component={Link}
+          to={"/read/" + book_id + "/" + prev_id}
+        >
+          فصل قبل - {prev_title}
+        </Button>
+      )}
+
+      <Container maxWidth="sm" className="chapter-content-container">
+        <Typography
+          variant="h4"
+          component="h3"
+          id="chapter-title"
+          className="chapter-title"
+        >
+          {chapter.title}
+        </Typography>
+        <Typography paragraph className="chapter-paragraph">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
+          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
+          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
+          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
+          قرار گیرد.
+        </Typography>
+        <Typography paragraph className="chapter-paragraph">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
+          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
+          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
+          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
+          قرار گیرد.
+        </Typography>
+        <Typography paragraph className="chapter-paragraph">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
+          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
+          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
+          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
+          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
+          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
+          قرار گیرد.
+        </Typography>
+      </Container>
+
+      {next_id && (
+        <Button
+          variant="outlined"
+          size="large"
+          color="primary"
+          className="chapter-change"
+          component={Link}
+          to={"/read/" + book_id + "/" + next_id}
+        >
+          فصل بعد - {next_title}
+        </Button>
+      )}
+
+      <ScrollTop {...props} target_id="chapter-title" />
+    </div>
+  );
+};
 
 export default ReadBook;
