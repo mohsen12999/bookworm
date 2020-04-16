7b2cbb129e368be481e6880d88197a5013963ea3
mohsen shabanian - work <mohsen1299@gmail.com>
2020-04-17 00:13:41 +0430

    add some todo

--- src/pages/admin/EditBlog.js
+++ src/pages/admin/EditBlog.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import { useParams } from "react-router-dom";
 
+// TODO: upload image
+// TODO: article name, summary and body
+
 const EditBlog = () => {
   const { blog_id } = useParams();
   return <div>Edit Blog page {blog_id}</div>;
--- src/pages/admin/EditNote.js
+++ src/pages/admin/EditNote.js
@@ -5,6 +5,10 @@ import { useParams } from "react-router-dom";
 // note_id == undefined => new note
 // note_id != undefined => load note
 
+// TODO: upload image
+// TODO: book name and summary
+// TODO: add, edit, remove chapter
+
 const EditNote = () => {
   const { note_id } = useParams();
   console.log("note_id", note_id);
