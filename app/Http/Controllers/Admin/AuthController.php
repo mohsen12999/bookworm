6a65b34662975a726f22a449078f5fce8e7b52d5
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-10 00:22:59 +0430

    add new route for login

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -35,4 +35,22 @@ class AuthController extends Controller
 
         return response()->json($user);
     }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:users,email', 
+            'password' => 'required'
+        ]);
+
+        if( Auth::attempt(['email'=>$request->email, 'password'=>$request->password]) ) {
+            $user = Auth::user();
+
+            $token = $user->createToken($user->email.'-'.now());
+
+            return response()->json([
+                'token' => $token->accessToken
+            ]);
+        }
+    }
 }
