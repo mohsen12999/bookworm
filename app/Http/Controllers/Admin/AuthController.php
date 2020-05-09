714ad92cbccffdebd612285cbcbcc99ee52f73c1
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-10 00:42:56 +0430

    fix problem in routing

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\User;
+use Auth;
 
 class AuthController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', 'AuthController@register');
-Route::post('login', 'AuthController@login');
+Route::post('register', 'Admin\AuthController@register');
+Route::post('login', 'Admin\AuthController@login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
