da77c9780d04ed6d1d55f96d7162b456a20a4496
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-12 12:53:10 +0430

    add public post query for post model

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
+
+    public function scopePublic_post($query)
+    {
+        return $query->where('publish_status', 100);
+    }
 }
