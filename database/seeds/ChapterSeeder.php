0a2a2305dcfda111a3175c617cdeb4dbbf2eba13
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-12 19:53:07 +0430

    add chapter seed

--- database/seeds/ChapterSeeder.php
+++ database/seeds/ChapterSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ChapterSeeder extends Seeder
 {
@@ -12,5 +13,16 @@ class ChapterSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('chapters')->insert(["title" => "فصل اول: مقدمه", "free_chapter" => 1, "book_id" => 1]);
+        DB::table('chapters')->insert(["title" => "فصل دوم: افتتاحیه", "free_chapter" => 0, "book_id" => 1]);
+        DB::table('chapters')->insert(["title" => "فصل سوم: اتفاق", "free_chapter" => 0, "book_id" => 1]);
+        DB::table('chapters')->insert(["title" => "فصل چهارم: مرگ", "free_chapter" => 0, "book_id" => 1]);
+        DB::table('chapters')->insert(["title" => "فصل پنجم: رستگاری", "free_chapter" => 0, "book_id" => 1]);
+
+        DB::table('chapters')->insert(["title" => "فصل اول: مقدمه", "free_chapter" => 1, "book_id" => 2]);
+        DB::table('chapters')->insert(["title" => "فصل دوم: افتتاحیه", "free_chapter" => 0, "book_id" => 2]);
+        DB::table('chapters')->insert(["title" => "فصل سوم: اتفاق", "free_chapter" => 0, "book_id" => 2]);
+        DB::table('chapters')->insert(["title" => "فصل چهارم: مرگ", "free_chapter" => 0, "book_id" => 2]);
+        DB::table('chapters')->insert(["title" => "فصل پنجم: رستگاری", "free_chapter" => 0, "book_id" => 2]);
     }
 }
