38dd6c808bbf780753f22b9eee505047415c4bae
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-13 01:32:08 +0430

    add function services

---
+++ resources/js/src/services/function.js
@@ -0,0 +1,19 @@
+export const getRandom = (arr, count) => {
+  let result = new Array(count);
+  if (!arr) {
+    return [];
+  }
+  let len = arr.length;
+  let taken = new Array(len);
+  if (count > len) {
+    return arr;
+  }
+  while (count--) {
+    let rand = Math.floor(Math.random() * len);
+    result[count] = arr[rand in taken ? taken[rand] : rand];
+    taken[rand] = --len in taken ? taken[len] : len;
+  }
+  return result;
+  // const shuffled = [...array].sort(() => 0.5 - Math.random())
+  // let selected = shuffled.slice(0, n);
+};
