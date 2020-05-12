56193004854ed304eed8c6a17820aa11e847f32f
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-12 21:26:31 +0430

    add publish status to chapter seeder

--- database/seeds/ChapterSeeder.php
+++ database/seeds/ChapterSeeder.php
@@ -13,16 +13,16 @@ class ChapterSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('chapters')->insert(["title" => "فصل اول: مقدمه", "free_chapter" => 1, "book_id" => 1]);
-        DB::table('chapters')->insert(["title" => "فصل دوم: افتتاحیه", "free_chapter" => 0, "book_id" => 1]);
-        DB::table('chapters')->insert(["title" => "فصل سوم: اتفاق", "free_chapter" => 0, "book_id" => 1]);
-        DB::table('chapters')->insert(["title" => "فصل چهارم: مرگ", "free_chapter" => 0, "book_id" => 1]);
-        DB::table('chapters')->insert(["title" => "فصل پنجم: رستگاری", "free_chapter" => 0, "book_id" => 1]);
+        DB::table('chapters')->insert(["title" => "فصل اول: مقدمه", "free_chapter" => 1, "book_id" => 1, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل دوم: افتتاحیه", "free_chapter" => 0, "book_id" => 1, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل سوم: اتفاق", "free_chapter" => 0, "book_id" => 1, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل چهارم: مرگ", "free_chapter" => 0, "book_id" => 1, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل پنجم: رستگاری", "free_chapter" => 0, "book_id" => 1, 'publish_status' => 100]);
 
-        DB::table('chapters')->insert(["title" => "فصل اول: مقدمه", "free_chapter" => 1, "book_id" => 2]);
-        DB::table('chapters')->insert(["title" => "فصل دوم: افتتاحیه", "free_chapter" => 0, "book_id" => 2]);
-        DB::table('chapters')->insert(["title" => "فصل سوم: اتفاق", "free_chapter" => 0, "book_id" => 2]);
-        DB::table('chapters')->insert(["title" => "فصل چهارم: مرگ", "free_chapter" => 0, "book_id" => 2]);
-        DB::table('chapters')->insert(["title" => "فصل پنجم: رستگاری", "free_chapter" => 0, "book_id" => 2]);
+        DB::table('chapters')->insert(["title" => "فصل اول: مقدمه", "free_chapter" => 1, "book_id" => 2, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل دوم: افتتاحیه", "free_chapter" => 0, "book_id" => 2, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل سوم: اتفاق", "free_chapter" => 0, "book_id" => 2, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل چهارم: مرگ", "free_chapter" => 0, "book_id" => 2, 'publish_status' => 100]);
+        DB::table('chapters')->insert(["title" => "فصل پنجم: رستگاری", "free_chapter" => 0, "book_id" => 2, 'publish_status' => 100]);
     }
 }
