714d5523afe3de55ae91b9886d38aab06e5e3bd4
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-16 22:16:40 +0430

    refresh token after get private data

--- resources/js/src/services/GetData.js
+++ resources/js/src/services/GetData.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import {
-  books,
-  articles,
-  genres,
-  authors,
-  subjects,
-  chapters,
+    books,
+    articles,
+    genres,
+    authors,
+    subjects,
+    chapters
 } from "./fakeData";
 
 const GET_PUBLIC_DATA_URL = "/api/get_data";
@@ -16,115 +16,118 @@ const PUBLIC_DATA = "publicData";
 const PRIVATE_DATA = "privateData";
 
 export const GetData = async () => {
-  fakeFillLocalStorage();
+    fakeFillLocalStorage();
 
-  const publicData = await getPublicData();
-  const privateData = await getPrivateData();
-  if (privateData) {
-    //publicData["user"] = privateData["user"];
-    publicData["chapter"] = privateData["chapter"];
-    return { publicData: publicData, privateData: privateData["user"] };
-  }
-  return { publicData: publicData, privateData: { isAuthenticated: false } };
+    const publicData = await getPublicData();
+    const privateData = await getPrivateData();
+    if (privateData) {
+        //publicData["user"] = privateData["user"];
+        publicData["chapter"] = privateData["chapter"];
+        return { publicData: publicData, privateData: privateData["user"] };
+    }
+    return { publicData: publicData, privateData: { isAuthenticated: false } };
 };
 
 const getPublicData = async () => {
-  try {
-    const response = await axios.get(GET_PUBLIC_DATA_URL);
-    const appData = response.data;
-    SavePublicDataToLocalStorage(appData);
+    try {
+        const response = await axios.get(GET_PUBLIC_DATA_URL);
+        const appData = response.data;
+        SavePublicDataToLocalStorage(appData);
 
-    return appData;
-  } catch (error) {
-    console.log("error:  getPublicData - ", error);
+        return appData;
+    } catch (error) {
+        console.log("error:  getPublicData - ", error);
 
-    return localStorage.getItem("appData")
-      ? JSON.parse(localStorage.getItem("appData"))
-      : {
-          user: { isAuthenticated: false },
-          genres: [],
-          books: [],
-          chapters: [],
-          posts: [],
-          subjects: [],
-          authors: [],
-        };
-  }
+        return localStorage.getItem("appData")
+            ? JSON.parse(localStorage.getItem("appData"))
+            : {
+                  user: { isAuthenticated: false },
+                  genres: [],
+                  books: [],
+                  chapters: [],
+                  posts: [],
+                  subjects: [],
+                  authors: []
+              };
+    }
 };
 
 const getPrivateData = async () => {
-  const token = localStorage.getItem(TOKEN);
-  if (!token) {
-    return undefined;
-  }
-  try {
-    const response = await axios.post(
-      GET_PRIVATE_DATA_URL,
-      {},
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.log("error:  getPrivateData -  ", error);
-    return undefined;
-  }
+    const token = localStorage.getItem(TOKEN);
+    if (!token) {
+        return undefined;
+    }
+    try {
+        const response = await axios.post(
+            GET_PRIVATE_DATA_URL,
+            {},
+            {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            }
+        );
+        const date = response.data;
+        AddToken(data.token);
+
+        return date;
+    } catch (error) {
+        console.log("error:  getPrivateData -  ", error);
+        return undefined;
+    }
 };
 
-export const AddToken = (token) => {
-  localStorage.setItem(TOKEN, token);
+export const AddToken = token => {
+    localStorage.setItem(TOKEN, token);
 };
 
 export const RemoveToken = () => {
-  localStorage.removeItem(TOKEN);
+    localStorage.removeItem(TOKEN);
 };
 
-export const SavePublicDataToLocalStorage = (appData) => {
-  localStorage.setItem(PUBLIC_DATA, JSON.stringify(appData));
+export const SavePublicDataToLocalStorage = appData => {
+    localStorage.setItem(PUBLIC_DATA, JSON.stringify(appData));
 };
 
-export const SavePrivateDataToLocalStorage = (appData) => {
-  localStorage.setItem(PRIVATE_DATA, JSON.stringify(appData));
+export const SavePrivateDataToLocalStorage = appData => {
+    localStorage.setItem(PRIVATE_DATA, JSON.stringify(appData));
 };
 
 export const RemovePrivateDataFromLocalStorage = () => {
-  localStorage.removeItem(PRIVATE_DATA);
+    localStorage.removeItem(PRIVATE_DATA);
 };
 
 const fakeFillLocalStorage = () => {
-  const appData = {
-    genres: genres,
-    books: books,
-    chapters: chapters,
-    subjects: subjects,
-    posts: articles,
-    authors: authors,
-    // snackbar: {
-    //   open: true,
-    //   time: 120000,
-    //   //severity: "success",
-    //   message: "موفق بود و بس",
-    // },
-  };
+    const appData = {
+        genres: genres,
+        books: books,
+        chapters: chapters,
+        subjects: subjects,
+        posts: articles,
+        authors: authors
+        // snackbar: {
+        //   open: true,
+        //   time: 120000,
+        //   //severity: "success",
+        //   message: "موفق بود و بس",
+        // },
+    };
 
-  const adminData = {
-    isAuthenticated: true,
-    name: "محسن",
-    email: "mohsen@gmail.com",
-    mobile: "09113923310",
-    avatar: "/images/user/default-profile.jpg",
-    wallet: 5000,
-    boughtBooks: [1, 7, 18],
-    writtenBooks: [],
-    writtenPosts: [],
-    lastBookId: 7,
-    lastChapterId: 103,
-  };
+    const adminData = {
+        isAuthenticated: true,
+        name: "محسن",
+        email: "mohsen@gmail.com",
+        mobile: "09113923310",
+        avatar: "/images/user/default-profile.jpg",
+        wallet: 5000,
+        boughtBooks: [1, 7, 18],
+        writtenBooks: [],
+        writtenPosts: [],
+        lastBookId: 7,
+        lastChapterId: 103
+    };
 
-  localStorage.getItem(PUBLIC_DATA) ?? SavePublicDataToLocalStorage(appData);
-  localStorage.getItem(PRIVATE_DATA) ??
-    SavePrivateDataToLocalStorage(adminData);
+    localStorage.getItem(PUBLIC_DATA) ?? SavePublicDataToLocalStorage(appData);
+    localStorage.getItem(PRIVATE_DATA) ??
+        SavePrivateDataToLocalStorage(adminData);
 };
