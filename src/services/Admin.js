c9f948b469d121e462cb9d7ed06575de4d3d8596
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-24 17:05:12 +0430

    fetch delete post

--- src/services/Admin.js
+++ src/services/Admin.js
@@ -3,45 +3,62 @@ import { AddToken, GetToken } from "./LocalStorage";
 
 const PROFILE_URL = "/api/profile";
 const NOTE_URL = "/api/note";
+const POST_URL = "/api/post";
 
-export const FetchUpdateProfile = async data => {
-    try {
-        const response = await axios.post(PROFILE_URL, data, {
-            headers: {
-                Authorization: "Bearer " + GetToken(),
-                "Content-type":
-                    "multipart/form-data; charset=utf-8; boundary=" +
-                    Math.random()
-                        .toString()
-                        .substr(2),
-                contentType: false,
-                processData: false
-            }
-        });
-        const responseData = response.data;
-        AddToken(responseData.token);
+export const FetchUpdateProfile = async (data) => {
+  try {
+    const response = await axios.post(PROFILE_URL, data, {
+      headers: {
+        Authorization: "Bearer " + GetToken(),
+        "Content-type":
+          "multipart/form-data; charset=utf-8; boundary=" +
+          Math.random().toString().substr(2),
+        contentType: false,
+        processData: false,
+      },
+    });
+    const responseData = response.data;
+    AddToken(responseData.token);
 
-        return { ...responseData, success: true };
-    } catch (error) {
-        console.log("error in FetchUpdateProfile", error);
-        return { success: false, error };
-    }
+    return { ...responseData, success: true };
+  } catch (error) {
+    console.log("error in FetchUpdateProfile", error);
+    return { success: false, error };
+  }
 };
 
-export const FetchDeleteNote = async noteId => {
-    try {
-        const response = await axios.delete(NOTE_URL + "/" + noteId, {
-            headers: {
-                Authorization: "Bearer " + GetToken()
-            }
-        });
+export const FetchDeleteNote = async (noteId) => {
+  try {
+    const response = await axios.delete(NOTE_URL + "/" + noteId, {
+      headers: {
+        Authorization: "Bearer " + GetToken(),
+      },
+    });
 
-        const responseData = response.data;
-        AddToken(responseData.token);
+    const responseData = response.data;
+    AddToken(responseData.token);
 
-        return { ...responseData, success: true };
-    } catch (error) {
-        console.log("error in FetchDeleteNote", error);
-        return { success: false, error };
-    }
+    return { ...responseData, success: true };
+  } catch (error) {
+    console.log("error in FetchDeleteNote", error);
+    return { success: false, error };
+  }
+};
+
+export const FetchDeletePost = async (noteId) => {
+  try {
+    const response = await axios.delete(POST_URL + "/" + noteId, {
+      headers: {
+        Authorization: "Bearer " + GetToken(),
+      },
+    });
+
+    const responseData = response.data;
+    AddToken(responseData.token);
+
+    return { ...responseData, success: true };
+  } catch (error) {
+    console.log("error in FetchDeleteNote", error);
+    return { success: false, error };
+  }
 };
