910f94d7883d3456fad60ce2c674a9b37a4f10e9
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-25 20:52:34 +0430

    add functions

--- resources/js/src/services/Admin.js
+++ resources/js/src/services/Admin.js
@@ -3,6 +3,7 @@ import { AddToken, GetToken } from "./LocalStorage";
 
 const PROFILE_URL = "/api/profile";
 const NOTE_URL = "/api/note";
+const BLOG_URL = "/api/blog";
 
 export const FetchUpdateProfile = async data => {
     try {
@@ -42,6 +43,48 @@ export const FetchDeleteNote = async noteId => {
         return { ...responseData, success: true };
     } catch (error) {
         console.log("error in FetchDeleteNote", error);
+        return { success: false, error };
+    }
+};
+
+export const FetchDeletePost = async postId => {
+    try {
+        const response = await axios.delete(BLOG_URL + "/" + postId, {
+            headers: {
+                Authorization: "Bearer " + GetToken()
+            }
+        });
+
+        const responseData = response.data;
+        AddToken(responseData.token);
+
+        return { ...responseData, success: true };
+    } catch (error) {
+        console.log("error in FetchDeleteNote", error);
+        return { success: false, error };
+    }
+};
+
+export const FetchWritePost = async data => {
+    try {
+        const response = await axios.post(BLOG_URL, data, {
+            headers: {
+                Authorization: "Bearer " + GetToken(),
+                "Content-type":
+                    "multipart/form-data; charset=utf-8; boundary=" +
+                    Math.random()
+                        .toString()
+                        .substr(2),
+                contentType: false,
+                processData: false
+            }
+        });
+        const responseData = response.data;
+        AddToken(responseData.token);
+
+        return { ...responseData, success: true };
+    } catch (error) {
+        console.log("error in FetchUpdateProfile", error);
         return { success: false, error };
     }
 };
