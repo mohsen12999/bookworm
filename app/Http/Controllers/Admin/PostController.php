5aa83e3f004a8d152d0c20f8f3445b17cd353e7d
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-26 01:22:46 +0430

    save post function on server

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Image;
 
 use App\Post;
+use App\Subject;
 
 class PostController extends Controller
 {
@@ -41,9 +42,91 @@ class PostController extends Controller
     {
         $user = $request->user();
 
-        $name = $request->name;
-        if (isset($name) && $name && $name != "undefined") {
-            $user->name = $name;
+        $id = $request->id;
+        $post = null;
+        if (isset($id) && $id && $id != "undefined") {
+            $find_post = Post::find($id);
+            if ($find_post && ($find_post->user_id == $user->id || $user->id == 100)) {
+                $post = $find_post;
+            }
         }
+
+        if (!$post) {
+            $post = new Post();
+        }
+
+        if (isset($_FILES["file"])) {
+            $file = $_FILES['file'];
+
+            $fileName = "post_" . $user->id . "_" . basename($file['name']);
+            $filePath = "/images/blog/";
+            $tmp = $file["tmp_name"];
+            $tmp_url = str_replace("\\", "/", $tmp);
+
+            //$moved = move_uploaded_file($tmp_url, $filePath . $fileName);
+            $image_resize = Image::make($tmp_url);
+            $image_resize->resize(540, 480);
+            $image_resize->save(public_path($filePath . $fileName));
+
+            if (file_exists(public_path($filePath . $fileName))) {
+                if (isset($post->img) && $post->img && file_exists(public_path($post->img))) {
+                    unlink(public_path($post->img));
+                }
+
+                $post->img = $filePath . $fileName;
+            }
+        }
+
+        $title = $request->title;
+        if (isset($title) && $title && $title != "undefined") {
+            $post->title = $title;
+        }
+
+        $abstract = $request->abstract;
+        if (isset($abstract) && $abstract && $abstract != "undefined") {
+            $post->abstract = $abstract;
+        }
+
+        $foreign_author = $request->foreign_author;
+        if (isset($foreign_author) && $foreign_author && $foreign_author != "undefined") {
+            $post->foreign_author = $foreign_author;
+        }
+
+        $description = $request->description;
+        if (isset($description) && $description && $description != "undefined") {
+            $post->description = $description;
+        }
+
+        //published
+        $published = $request->published;
+        if (isset($published) && $published && $published != "undefined" && $published != "false" && $published != "False") {
+            $post->published = true;
+        } else {
+            $post->published = false;
+        }
+
+        $subject = $request->subject;
+        if (isset($subject) && $subject && $subject != "undefined") {
+            $subject = trim($subject);
+            $sub = Subject::where("title", "like", '%' . $subject . '%');
+            if (!$sub) {
+                $sub = new Subject();
+                $sub->title = $subject;
+                $sub->save();
+            }
+            $post->subject_id = $sub->id;
+        }
+
+        if (!$post->user_id) {
+            $post->user_id = $user->id;
+        }
+
+        $post->save();
+
+        return response()->json([
+            'token' => $this->generateAccessToken($user),
+            'post' => $post,
+            //'request' => $request->all(),
+        ], 200);
     }
 }
