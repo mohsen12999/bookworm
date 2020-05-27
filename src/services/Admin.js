0dfd947606e4ec0bf112b577fe8e9be3081a3447
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-28 02:20:26 +0430

    add FetchWriteChapter to admin

--- src/services/Admin.js
+++ src/services/Admin.js
@@ -125,3 +125,25 @@ export const FetchWriteBook = async (data) => {
     return { success: false, error };
   }
 };
+
+export const FetchWriteChapter = async (data) => {
+  try {
+    const response = await axios.post(CHAPTER_URL, data, {
+      headers: {
+        Authorization: "Bearer " + GetToken(),
+        "Content-type":
+          "multipart/form-data; charset=utf-8; boundary=" +
+          Math.random().toString().substr(2),
+        contentType: false,
+        processData: false,
+      },
+    });
+    const responseData = response.data;
+    AddToken(responseData.token);
+
+    return { ...responseData, success: true };
+  } catch (error) {
+    console.log("error in FetchUpdateProfile", error);
+    return { success: false, error };
+  }
+};
