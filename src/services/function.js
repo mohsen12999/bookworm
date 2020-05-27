1869e1476ef7c31c8f95450fda10456e8f63e2f5
mohsen shabanian - work <mohsen1299@gmail.com>
2020-05-28 01:38:33 +0430

    chapter status function

--- src/services/function.js
+++ src/services/function.js
@@ -46,3 +46,12 @@ export const BookPublishStatusDescription = (publish_status) =>
     : publish_status === 100
     ? "منتشر شده"
     : "";
+
+export const ChapterPublishStatusDescription = (publish_status) =>
+  publish_status === 0
+    ? "ذخیره شده"
+    : publish_status === 10
+    ? "آماده انتشار"
+    : publish_status === 100
+    ? "منتشر شده"
+    : "";
