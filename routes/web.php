d60cd186b461b23f67fefccd0805f49c6bef86d0
mohsen shabanian - work <mohsen1299@gmail.com>
2020-06-07 11:32:12 +0430

    change route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{path?}', 'app');
+//Route::view('/{path?}', 'app');
+Route::view('/{path?}', 'app')->where('path', '.*');
+// Route::get('/{any}', 'app')->where('any', '.*');
+// Route::get('/{any?}', 'app')->where('any', '^(?!api\/)[\/\w\.-]*');
 
 // Route::get('/', function () {
 //     return view('welcome');
