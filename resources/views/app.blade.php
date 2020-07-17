5521eddc897bdb4d56950619db8518f9b303036f
mohsen shabanian - work <mohsen1299@gmail.com>
2020-07-17 22:56:51 +0430

    add new things to for better show

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>کرم کتاب</title>
+        <title>کتابچه</title>
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json" />
     </head>
     <body>
         <div id="root"></div>
