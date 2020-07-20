4e00c6540d6e9aa307e4d82e1a03e6f07f67f9f4
mohsen shabanian - work <mohsen1299@gmail.com>
2020-07-20 18:25:30 +0430

    change name

--- resources/js/src/components/MainLayout.js
+++ resources/js/src/components/MainLayout.js
@@ -15,121 +15,121 @@ import MyBackdrop from "./MyBackdrop";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  drawer: {
-    [theme.breakpoints.up("sm")]: {
-      width: drawerWidth,
-      flexShrink: 0,
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex"
     },
-  },
-  appBar: {
-    [theme.breakpoints.up("sm")]: {
-      // width: `calc(100% - ${drawerWidth}px)`,
-      // marginLeft: drawerWidth
-      zIndex: 1201,
+    drawer: {
+        [theme.breakpoints.up("sm")]: {
+            width: drawerWidth,
+            flexShrink: 0
+        }
     },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
+    appBar: {
+        [theme.breakpoints.up("sm")]: {
+            // width: `calc(100% - ${drawerWidth}px)`,
+            // marginLeft: drawerWidth
+            zIndex: 1201
+        }
     },
-  },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  title: {
-    flexGrow: 1,
-    textAlign: "right",
-    // marginRight: drawerWidth
-    [theme.breakpoints.up("sm")]: {
-      textAlign: "center",
-      fontSize: "2rem",
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up("sm")]: {
+            display: "none"
+        }
     },
-  },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3)
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: "right",
+        // marginRight: drawerWidth
+        [theme.breakpoints.up("sm")]: {
+            textAlign: "center",
+            fontSize: "2rem"
+        }
+    }
 }));
 
-const MainLayout = (props) => {
-  const { container } = props;
-  const classes = useStyles();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+const MainLayout = props => {
+    const { container } = props;
+    const classes = useStyles();
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap className={classes.title}>
-            کرم کتاب
-          </Typography>
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" noWrap className={classes.title}>
+                        کتابچه
+                    </Typography>
 
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={handleDrawerToggle}
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
 
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        {props.children}
-      </main>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                {props.children}
+            </main>
 
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor="right"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            <DrawerMenu />
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            anchor="right"
-            variant="permanent"
-            open
-          >
-            <DrawerMenu />
-          </Drawer>
-        </Hidden>
-      </nav>
-      <MySnackbar />
-      <MyBackdrop />
-    </div>
-  );
-  //return <React.Fragment>{props.children}</React.Fragment>;
+            <nav className={classes.drawer} aria-label="mailbox folders">
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        anchor="right"
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        classes={{
+                            paper: classes.drawerPaper
+                        }}
+                        ModalProps={{
+                            keepMounted: true // Better open performance on mobile.
+                        }}
+                    >
+                        <DrawerMenu />
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer
+                        classes={{
+                            paper: classes.drawerPaper
+                        }}
+                        anchor="right"
+                        variant="permanent"
+                        open
+                    >
+                        <DrawerMenu />
+                    </Drawer>
+                </Hidden>
+            </nav>
+            <MySnackbar />
+            <MyBackdrop />
+        </div>
+    );
+    //return <React.Fragment>{props.children}</React.Fragment>;
 };
 
 export default MainLayout;
