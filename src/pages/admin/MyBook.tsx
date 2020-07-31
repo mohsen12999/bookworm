66b7bc2a85e47f93686d4772f2ef55cd9a4976fa
mohsen shabanian - work <mohsen1299@gmail.com>
2020-07-31 23:02:01 +0430

    change name

--- src/pages/admin/MyBook.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const MyBook = () => <div></div>;
-
-export default MyBook;
--- src/pages/admin/MyPost.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const MyPost = () => <div></div>;
-
-export default MyPost;
