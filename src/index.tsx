949f86d74b8ad9e3062a983a6fd83345e5d0b4b0
mohsen shabanian - work <mohsen1299@gmail.com>
2020-07-31 22:32:18 +0430

    add some route

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import configureStore, { history } from "./store/configureStore";
 import { Switch, Route } from "react-router";
+
+import "./index.css";
+
+import { HomePages, AuthPages, AdminPages } from "./constants/pages";
+
+import Index from "./pages/public/Index";
+import BookList from "./pages/public/BookList";
+import PostList from "./pages/public/PostList";
+import About from "./pages/public/About";
+import NotFound from "./pages/public/NotFound";
+import SingleBook from "./pages/public/SingleBook";
+import SinglePost from "./pages/public/SinglePost";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Logout from "./pages/auth/Logout";
 
 const store = configureStore({} /* provide initial state if any */);
 
@@ -16,10 +29,64 @@ ReactDOM.render(
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <Switch>
-          <Route component={App} />
-          <Route component={App} />
-          <Route component={App} />
-          <Route component={App} />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + HomePages.INDEX}
+            component={Index}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + HomePages.BOOKS}
+            component={BookList}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + HomePages.POSTS}
+            component={PostList}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + HomePages.ABOUT}
+            component={About}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + HomePages.BOOK + "/:id"}
+            component={SingleBook}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + HomePages.POST + "/:id"}
+            component={SinglePost}
+          />
+          <Route
+            exact
+            path={
+              process.env.PUBLIC_URL +
+              "/" +
+              HomePages.READ +
+              "/:book_id/:chapter_id"
+            }
+            component={PostList}
+          />
+
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + AuthPages.REGISTER}
+            component={Register}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + AuthPages.LOGIN}
+            component={Login}
+          />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/" + AuthPages.LOGOUT}
+            component={Logout}
+          />
+
+          <Route component={NotFound} />
         </Switch>
       </ConnectedRouter>
     </Provider>
