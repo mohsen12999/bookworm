64a700199ab59eb90714b9deef47c1f9a7e06f5d
mohsen shabanian - work <mohsen1299@gmail.com>
2020-07-31 20:54:30 +0430

    remove comment

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -22,26 +22,3 @@ export default function configureStore(preloadedState: any) {
 
   return store;
 }
-
-// import { createStore, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
-
-// import { createBrowserHistory } from "history";
-// import { routerMiddleware } from "connected-react-router";
-// import thunk from "redux-thunk";
-// import combineReducers from "../reducers/combineReducers";
-
-// export const history = createBrowserHistory();
-
-// const composeEnhancers = composeWithDevTools(
-//   applyMiddleware(
-//     routerMiddleware(history), // for dispatching history actions
-//     thunk
-//   )
-//   // options like actionSanitizer, stateSanitizer
-// );
-
-// export const configureStore = (preloadedState: any) =>
-//   createStore(combineReducers, preloadedState, composeEnhancers);
-
-// export default configureStore;
