1039bb1d279cd4c7292c2655a7fd9695e1718f7f
mohsen shabanian - work <mohsen1299@gmail.com>
2020-07-31 19:22:13 +0430

    APP Page Name

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -1,4 +1,26 @@
-export enum AppPage {
-  Main,
-  About,
+export enum HomePages {
+  INDEX = "index",
+  ABOUT = "about",
+  BOOKS = "books", // book list
+  BOOK = "book", // book info page
+  READ = "read", // show single chapter
+  BLOGS = "blogs", // blog list
+  BLOG = "blog",
+}
+
+export enum AuthPages {
+  LOGIN = "login",
+  LOGOUT = "logout",
+  REGISTER = "register",
+}
+
+export enum AdminPages {
+  DASHBOARD = "dashboard",
+  PROFILE = "profile",
+  WALLET = "wallet",
+  MY_BOOKS = "mybooks",
+  MY_BOOK = "mybook",
+  MY_CHAPTER = "mychapter",
+  MY_BLOGS = "myblogs",
+  MY_BLOG = "myblog",
 }
