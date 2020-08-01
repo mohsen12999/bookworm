22ad8d475086c8018c1e843100b11f7252d9591f
Mohsen Shabanian <mohsen1299@gmail.com>
2020-08-01 17:38:39 +0430

    work with scrollbar

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -4,8 +4,15 @@ import Zoom from "@material-ui/core/Zoom";
 import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
-const ScrollTop = (props) => {
-  const { window, target_id } = props;
+import "./ScrollTop.css";
+
+interface IScrollTopProps {
+  target_id: string;
+  window: any; // Window
+}
+
+const ScrollTop = ({ window, target_id }: IScrollTopProps) => {
+  // const { window, target_id } = props;
 
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
@@ -13,10 +20,9 @@ const ScrollTop = (props) => {
     threshold: 100,
   });
 
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector(
-      "#" + target_id
-    );
+  const handleClick = (event: React.MouseEvent) => {
+    //const anchor = (event.target.ownerDocument || document).querySelector(
+    const anchor = document.querySelector("#" + target_id);
 
     if (anchor) {
       anchor.scrollIntoView({ behavior: "smooth", block: "center" });
@@ -25,11 +31,7 @@ const ScrollTop = (props) => {
 
   return (
     <Zoom in={trigger}>
-      <div
-        onClick={handleClick}
-        role="presentation"
-        style={{ position: "fixed", bottom: "5px", left: "5px" }}
-      >
+      <div onClick={handleClick} role="presentation" className="top-arrow-div">
         <Fab color="secondary" size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon />
         </Fab>
