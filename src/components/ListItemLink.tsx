b35ea7cd91cc6b52a84f91318a55be10de1d0d61
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-02 19:52:47 +0430

    dont use react use memp

--- src/components/ListItemLink.tsx
+++ src/components/ListItemLink.tsx
@@ -8,24 +8,30 @@ import Tooltip from "@material-ui/core/Tooltip";
 import "./ListItemLink.css";
 
 interface IListItemLinkProps {
-	icon: JSX.Element;
-	primary:string;
-	to:string;
-	enable:boolean;
-	exactMatch:boolean;
+  icon: JSX.Element;
+  primary: string;
+  to: string;
+  enable: boolean;
+  exactMatch: boolean;
 }
 
-function ListItemLink({icon, primary, to, enable, exactMatch}:IListItemLinkProps) {
-	//  const { icon, primary, to, enable, exactMatch } = props;
+function ListItemLink({
+  icon,
+  primary,
+  to,
+  enable,
+  exactMatch,
+}: IListItemLinkProps) {
+  //  const { icon, primary, to, enable, exactMatch } = props;
 
-  const renderLink = React.useMemo(
-    () =>
-      React.forwardRef((linkProps, ref) => (
-	      {/*<Link ref={ref} to={enable ? to : "/login"} {...linkProps} />*/}
-	      <Link ref={ref as React.MutableRefObject<HTMLInputElement>} to={enable ? to : "/login"} {...linkProps} />
-      )),
-    [to, enable]
-  );
+  // const renderLink = React.useMemo(
+  //   () =>
+  //     React.forwardRef((linkProps, ref) => (
+  //       {/*<Link ref={ref} to={enable ? to : "/login"} {...linkProps} />*/}
+  //       <Link ref={(ref as React.MutableRefObject<HTMLInputElement>)} to={enable ? to : "/login"} {...linkProps} />
+  //     )),
+  //   [to, enable]
+  // );
 
   const match = useRouteMatch({ path: to, exact: exactMatch });
 
@@ -37,7 +43,8 @@ function ListItemLink({icon, primary, to, enable, exactMatch}:IListItemLinkProps
       >
         <ListItem
           button
-          component={renderLink}
+          component={Link}
+          to={enable ? to : "/login"}
           className={match ? "active-link" : enable ? "link" : "disable-link"}
         >
           <ListItemIcon>{icon}</ListItemIcon>
