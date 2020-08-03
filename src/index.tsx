9118db8214893459329b7578d6dd3c97769b6913
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-03 14:36:20 +0430

    use app file for routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import configureStore, { history } from "./store/configureStore";
-import { Switch, Route } from "react-router";
 
 import "./index.css";
-
-import { HomePages, AuthPages, AdminPages } from "./constants/pages";
-
-import Index from "./pages/public/Index";
-import BookList from "./pages/public/BookList";
-import PostList from "./pages/public/PostList";
-import About from "./pages/public/About";
-import NotFound from "./pages/public/NotFound";
-import SingleBook from "./pages/public/SingleBook";
-import SinglePost from "./pages/public/SinglePost";
-import Register from "./pages/auth/Register";
-import Login from "./pages/auth/Login";
-import Logout from "./pages/auth/Logout";
-import Search from "./pages/public/Search";
-import Dashboard from "./pages/admin/Dashboard";
-import Profile from "./pages/admin/Profile";
-import MyBooks from "./pages/admin/MyBooks";
-import MyPosts from "./pages/admin/MyPosts";
-import MyLibrary from "./pages/admin/MyLibrary";
-import Wallet from "./pages/admin/Wallet";
-import PrivateRouteLayout from "./components/PrivateRouteLayout";
-import EditPost from "./pages/admin/EditPost";
-import EditBook from "./pages/admin/EditBook";
-import EditChapter from "./pages/admin/EditChapter";
+import App from "./pages/App";
 
 const store = configureStore({} /* provide initial state if any */);
 
@@ -39,141 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Switch>
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.INDEX}
-            component={Index}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.BOOKS}
-            component={BookList}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.POSTS}
-            component={PostList}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.ABOUT}
-            component={About}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.SEARCH}
-            component={Search}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.BOOK + "/:id"}
-            component={SingleBook}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + HomePages.POST + "/:id"}
-            component={SinglePost}
-          />
-          <Route
-            exact
-            path={
-              process.env.PUBLIC_URL +
-              "/" +
-              HomePages.READ +
-              "/:book_id/:chapter_id"
-            }
-            component={PostList}
-          />
-
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AuthPages.REGISTER}
-            component={Register}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AuthPages.LOGIN}
-            component={Login}
-          />
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AuthPages.LOGOUT}
-            component={Logout}
-          />
-
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AdminPages.DASHBOARD}
-            component={Dashboard}
-          />
-          <Route exact path={process.env.PUBLIC_URL + "/" + AdminPages.PROFILE}>
-            <PrivateRouteLayout>
-              <Profile />
-            </PrivateRouteLayout>
-          </Route>
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AdminPages.MY_BOOKS}
-          >
-            <PrivateRouteLayout>
-              <MyBooks />
-            </PrivateRouteLayout>
-          </Route>
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AdminPages.MY_POSTS}
-          >
-            <PrivateRouteLayout>
-              <MyPosts />
-            </PrivateRouteLayout>
-          </Route>
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AdminPages.MY_Library}
-          >
-            <PrivateRouteLayout>
-              <MyLibrary />
-            </PrivateRouteLayout>
-          </Route>
-          <Route exact path={process.env.PUBLIC_URL + "/" + AdminPages.WALLET}>
-            <PrivateRouteLayout>
-              <Wallet />
-            </PrivateRouteLayout>
-          </Route>
-
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AdminPages.EDIT_POST + "/:id?"}
-          >
-            <PrivateRouteLayout>
-              <EditPost />
-            </PrivateRouteLayout>
-          </Route>
-          <Route
-            exact
-            path={process.env.PUBLIC_URL + "/" + AdminPages.EDIT_BOOK + "/:id?"}
-          >
-            <PrivateRouteLayout>
-              <EditBook />
-            </PrivateRouteLayout>
-          </Route>
-          <Route
-            exact
-            path={
-              process.env.PUBLIC_URL +
-              "/" +
-              AdminPages.EDIT_CHAPTER +
-              "/:book_id/:chapter_id?"
-            }
-          >
-            <PrivateRouteLayout>
-              <EditChapter />
-            </PrivateRouteLayout>
-          </Route>
-
-          <Route component={NotFound} />
-        </Switch>
+        <App />
       </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
---
+++ src/pages/App.tsx
@@ -0,0 +1,170 @@
+import React from "react";
+import { Switch, Route } from "react-router";
+
+import { HomePages, AuthPages, AdminPages } from "../constants/pages";
+
+import MainLayout from "../components/MainLayout";
+import PrivateRouteLayout from "../components/PrivateRouteLayout";
+
+import Index from "./public/Index";
+import BookList from "./public/BookList";
+import PostList from "./public/PostList";
+import About from "./public/About";
+import NotFound from "./public/NotFound";
+import SingleBook from "./public/SingleBook";
+import SinglePost from "./public/SinglePost";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import Logout from "./auth/Logout";
+import Search from "./public/Search";
+import Dashboard from "./admin/Dashboard";
+import Profile from "./admin/Profile";
+import MyBooks from "./admin/MyBooks";
+import MyPosts from "./admin/MyPosts";
+import MyLibrary from "./admin/MyLibrary";
+import Wallet from "./admin/Wallet";
+import EditPost from "./admin/EditPost";
+import EditBook from "./admin/EditBook";
+import EditChapter from "./admin/EditChapter";
+
+function App() {
+  React.useEffect(() => {
+    // TODO: load info from server
+    console.log("first start");
+  }, []);
+  return (
+    <MainLayout>
+      <Switch>
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.INDEX}
+          component={Index}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.BOOKS}
+          component={BookList}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.POSTS}
+          component={PostList}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.ABOUT}
+          component={About}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.SEARCH}
+          component={Search}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.BOOK + "/:id"}
+          component={SingleBook}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + HomePages.POST + "/:id"}
+          component={SinglePost}
+        />
+        <Route
+          exact
+          path={
+            process.env.PUBLIC_URL +
+            "/" +
+            HomePages.READ +
+            "/:book_id/:chapter_id"
+          }
+          component={PostList}
+        />
+
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AuthPages.REGISTER}
+          component={Register}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AuthPages.LOGIN}
+          component={Login}
+        />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AuthPages.LOGOUT}
+          component={Logout}
+        />
+
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AdminPages.DASHBOARD}
+          component={Dashboard}
+        />
+        <Route exact path={process.env.PUBLIC_URL + "/" + AdminPages.PROFILE}>
+          <PrivateRouteLayout>
+            <Profile />
+          </PrivateRouteLayout>
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/" + AdminPages.MY_BOOKS}>
+          <PrivateRouteLayout>
+            <MyBooks />
+          </PrivateRouteLayout>
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/" + AdminPages.MY_POSTS}>
+          <PrivateRouteLayout>
+            <MyPosts />
+          </PrivateRouteLayout>
+        </Route>
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AdminPages.MY_Library}
+        >
+          <PrivateRouteLayout>
+            <MyLibrary />
+          </PrivateRouteLayout>
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/" + AdminPages.WALLET}>
+          <PrivateRouteLayout>
+            <Wallet />
+          </PrivateRouteLayout>
+        </Route>
+
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AdminPages.EDIT_POST + "/:id?"}
+        >
+          <PrivateRouteLayout>
+            <EditPost />
+          </PrivateRouteLayout>
+        </Route>
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/" + AdminPages.EDIT_BOOK + "/:id?"}
+        >
+          <PrivateRouteLayout>
+            <EditBook />
+          </PrivateRouteLayout>
+        </Route>
+        <Route
+          exact
+          path={
+            process.env.PUBLIC_URL +
+            "/" +
+            AdminPages.EDIT_CHAPTER +
+            "/:book_id/:chapter_id?"
+          }
+        >
+          <PrivateRouteLayout>
+            <EditChapter />
+          </PrivateRouteLayout>
+        </Route>
+
+        <Route component={NotFound} />
+      </Switch>
+    </MainLayout>
+  );
+}
+
+export default App;
