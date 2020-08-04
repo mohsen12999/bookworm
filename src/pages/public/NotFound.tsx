9487cb68054738a02df6cb62afd40149e1d329d1
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-04 14:23:27 +0430

    make notfound page

--- src/pages/public/NotFound.tsx
+++ src/pages/public/NotFound.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import { HomePages } from "../../constants/pages";
 
-const NotFound = () => <div></div>;
+import "./NotFound.css";
+
+const NotFound = () => (
+  <div className="not-found">
+    <h2>متاسفانه صفحه مورد نظر پیدا نشد.</h2>
+    <Button
+      variant="contained"
+      color="primary"
+      component={Link}
+      to={"/" + HomePages.INDEX}
+    >
+      بازگشت به صفحه اول
+    </Button>
+  </div>
+);
 
 export default NotFound;
