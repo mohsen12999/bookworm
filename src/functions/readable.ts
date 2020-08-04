3510aa9c963ace4f78bc7ed3f11dd5ad03668a0a
Mohsen Shabanian <mohsen1299@gmail.com>
2020-08-05 01:13:59 +0430

    fix readable function

--- src/functions/readable.ts
+++ src/functions/readable.ts
@@ -1,19 +1,43 @@
-export const readableBookFromBook = (book:IBook,boughtBook:number[]){
-	// free or bought
-	if(book.price==0 || boughtBook.includes(book.id) ){
-		return true;
-	}
-	return false;
-}
+import { IBook, IChapter } from "../types/publicTypes";
 
-export const readableBook = (books:IBook[],book_id:number,boughtBook:number[]){
-	const book = books.find((b)=>b.id==book_id);
-	return readableBookFromBook(book,boughtBook);
-}
+export const readableBookFromBook = (book: IBook, boughtBook: number[]) => {
+  // free or bought
+  if (book.price == 0 || boughtBook.includes(book.id)) {
+    return true;
+  }
+  return false;
+};
 
-export const readableChapter(chapter:IChapter,book:IBook,boughtBook:number[]){
-	if(chapter.free || book.price ==0 || boughtBook(book_id)){
-		return true;
-	}
-	return false;
-}
+export const readableBook = (
+  books: IBook[],
+  book_id: number,
+  boughtBook: number[]
+) => {
+  const book = books.find((b) => b.id == book_id);
+  return book ? readableBookFromBook(book, boughtBook) : false;
+};
+
+export const readableChapterFromBook = (
+  chapter: IChapter,
+  book: IBook,
+  boughtBook: number[]
+) => {
+  if (chapter.free || book.price == 0 || boughtBook.includes(book.id)) {
+    return true;
+  }
+  return false;
+};
+
+export const readableChapter = (
+  chapter: IChapter,
+  books: IBook[],
+  book_id: number,
+  boughtBook: number[]
+) => {
+  const book = books.find((b) => b.id == book_id);
+  if (!book) return false;
+  if (chapter.free || book.price == 0 || boughtBook.includes(book_id)) {
+    return true;
+  }
+  return false;
+};
