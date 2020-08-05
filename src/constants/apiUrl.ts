04a62f626ea6ef841f71f734c4255536c9ad4d92
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-05 21:02:34 +0430

    url for connect to server

---
+++ src/constants/apiUrl.ts
@@ -0,0 +1,12 @@
+export enum AuthUrl {
+  LOGIN_URL = "/api/login",
+  REG_URL = "/api/register",
+}
+
+export enum AdminUrl {
+  PROFILE_URL = "/api/profile",
+  NOTE_URL = "/api/note",
+  BLOG_URL = "/api/blog",
+  CHAPTER_URL = "/api/chapter",
+  Get_Data_URL = "/api/get_data",
+}
