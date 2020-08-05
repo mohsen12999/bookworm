b1d7cbb6ece6a765ad5df3b9e45ed1190a6882a2
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-05 22:31:34 +0430

    add action type and interface

--- src/constants/actionTypes.ts
+++ src/constants/actionTypes.ts
@@ -1,12 +1,30 @@
 export enum AppActionType {
   CLOSE_SNACKBAR = "closeSnackbar",
-}
-
-export interface AppAction {
-  type: AppActionType;
-  payload: any;
+  START_LOADING = "startLoading",
+  STOP_LOADING = "stopLoading",
 }
 
 export enum AdminActionType {
   SET_LAST_BOOK_READING = "setLastBookReading",
+}
+
+export enum AuthActionType {
+  LOGIN = "login",
+  SUCCESS_LOGIN = "successLogin",
+  FAILED_LOGIN = "failedLogin",
+}
+
+interface IAction {
+  type: any;
+  payload?: any;
+}
+
+export interface IAppAction extends IAction {
+  type: AppActionType;
+  payload?: undefined;
+}
+
+export interface IAdminAction extends IAction {
+  type: AdminActionType;
+  payload?: undefined | { book_id: number; chapter_id: number };
 }
