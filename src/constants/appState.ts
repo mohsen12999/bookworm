35d7cd5eebb8489462398d3f27c6e9f21255af0d
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-05 14:27:05 +0430

    lettle change in line view

--- src/constants/appState.ts
+++ src/constants/appState.ts
@@ -1,3 +1,6 @@
 import { IAppState } from "../types/appType";
 
-export const appInitialState: IAppState = { loading: false,snackbar:{open:false,message:"",time:6000} }; 
+export const appInitialState: IAppState = {
+  loading: false,
+  snackbar: { open: false, message: "", time: 6000 },
+};
