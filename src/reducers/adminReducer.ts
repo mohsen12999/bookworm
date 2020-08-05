f1913982dbce692e0c9245ed9f605bf0e042f8d1
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-05 22:32:21 +0430

    add reducer to admin reducer

--- src/reducers/adminReducer.ts
+++ src/reducers/adminReducer.ts
@@ -1,11 +1,22 @@
 import { IAdminState } from "../types/adminType";
 import { adminInitialState } from "../constants/adminState";
+import { IAdminAction, AdminActionType } from "../constants/actionTypes";
 
 const AdminReducer: any = (
   state: IAdminState = adminInitialState,
-  action: any
+  action: IAdminAction
 ) => {
-  return state;
+  switch (action.type) {
+    case AdminActionType.SET_LAST_BOOK_READING:
+      return {
+        ...state,
+        lastBookId: action.payload?.book_id,
+        lastChapterId: action.payload?.chapter_id,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default AdminReducer;
