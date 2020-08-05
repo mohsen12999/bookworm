df04614f5c8d557a34341fc201ac5e24139e735e
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-05 22:32:37 +0430

    add some reducer

--- src/reducers/appReducer.ts
+++ src/reducers/appReducer.ts
@@ -1,11 +1,16 @@
 import { IAppState } from "../types/appType";
-import { AppAction, AppActionType } from "../constants/actionTypes";
+import { IAppAction, AppActionType } from "../constants/actionTypes";
 import { appInitialState } from "../constants/appState";
 
-const AppReducer = (state: IAppState = appInitialState, action: AppAction) => {
+const AppReducer = (state: IAppState = appInitialState, action: IAppAction) => {
   switch (action.type) {
     case AppActionType.CLOSE_SNACKBAR:
       return { ...state, snackbar: { ...state.snackbar, open: false } };
+    case AppActionType.START_LOADING:
+      return { ...state, loading: true };
+
+    case AppActionType.STOP_LOADING:
+      return { ...state, loading: false };
 
     default:
       return state;
