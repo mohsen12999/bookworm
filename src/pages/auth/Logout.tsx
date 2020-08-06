2338dad3fcdadc117296642755741da2e49719b3
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-06 12:06:21 +0430

    make logout page

--- src/pages/auth/Logout.tsx
+++ src/pages/auth/Logout.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Logout = () => <div></div>;
+import { loggingOut } from "../../actions/AuthAction";
 
-export default Logout;
+interface ILogoutProps {
+  loggingOut(): void;
+}
+const Logout = (props: ILogoutProps) => {
+  React.useEffect(() => {
+    loggingOut();
+  }, []);
+
+  return <Redirect to={"/"} />;
+};
+
+const mapStateToProps = (State: any) => {};
+
+const mapDispatchToProps = {
+  loggingOut,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Logout);
