0ebdeb0822696bfba4c1529fb4aa5df9a7901e17
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-06 12:05:49 +0430

    add register function

--- src/actions/AuthAction.ts
+++ src/actions/AuthAction.ts
@@ -9,12 +9,12 @@ export const tryToLogin = (email?: string, password?: string) => (
     type: AppActionType.START_LOADING,
   });
 
-  //send
+  // TODO: try to login
 
   // result
-  const result = true;
+  const result: boolean = Math.random() > 0.5;
   dispatch({
-    type: AppActionType.STOP_LOADING,
+    type: AppActionType.STOP_LOADING_AND_MESSAGE,
     payload: { msg: result ? "خوش آمدید" : "اشکال در ورود" },
   });
 
@@ -29,4 +29,47 @@ export const tryToLogin = (email?: string, password?: string) => (
       payload: { error: "" },
     });
   }
+};
+
+export const tryToRegister = (
+  name?: string,
+  email?: string,
+  password?: string,
+  password_confirmation?: string
+) => (dispatch: Dispatch) => {
+  // loading
+  dispatch({
+    type: AppActionType.START_LOADING,
+  });
+
+  // TODO: try to register
+
+  //
+  const result: boolean = Math.random() > 0.5;
+  dispatch({
+    type: AppActionType.STOP_LOADING_AND_MESSAGE,
+    payload: {
+      msg: result ? "ثبت نام موفق بود، لطفا وارد شوید" : "اشکال در ثبت نام",
+    },
+  });
+
+  if (result) {
+    dispatch({
+      type: AdminActionType.SUCCESS_REGISTER,
+      payload: { data: "" },
+    });
+  } else {
+    dispatch({
+      type: AdminActionType.FAILED_REGISTER,
+      payload: { error: "" },
+    });
+  }
+
+  return result;
+};
+
+export const loggingOut = () => (dispatch: Dispatch) => {
+  dispatch({
+    type: AdminActionType.LOGGING_OUT,
+  });
 };
