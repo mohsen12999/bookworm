d1fc0c6978116522b991f18e0f36376b7eba4698
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-07 22:31:25 +0430

    remove duplicate function

--- src/functions/publishStatus.ts
+++ src/functions/publishStatus.ts
@@ -1,22 +1,4 @@
-export const PostPublishStatusDescription = (publish_status: number) =>
-  publish_status === 0
-    ? "ذخیره شده"
-    : publish_status === 10
-    ? "آماده انتشار"
-    : publish_status === 100
-    ? "منتشر شده"
-    : "";
-
-export const BookPublishStatusDescription = (publish_status: number) =>
-  publish_status === 0
-    ? "ذخیره شده"
-    : publish_status === 10
-    ? "آماده انتشار"
-    : publish_status === 100
-    ? "منتشر شده"
-    : "";
-
-export const ChapterPublishStatusDescription = (publish_status: number) =>
+export const publishStatusDescription = (publish_status: number) =>
   publish_status === 0
     ? "ذخیره شده"
     : publish_status === 10
--- src/functions/writeStatus.ts
+++ src/functions/writeStatus.ts
@@ -1,34 +1,4 @@
-export const PostWriteStatusDescription = (publish_status: number) =>
-  publish_status === 0
-    ? "ذخیره شده"
-    : publish_status === 10
-    ? "اتمام نوشته"
-    : publish_status === 40
-    ? "تغییر کرده"
-    : publish_status === 50
-    ? "دارای نقص"
-    : publish_status === 70
-    ? "در حال بررسی"
-    : publish_status === 100
-    ? "منتشر شده"
-    : "";
-
-export const BookWriteStatusDescription = (publish_status: number) =>
-  publish_status === 0
-    ? "ذخیره شده"
-    : publish_status === 10
-    ? "اتمام نوشته"
-    : publish_status === 40
-    ? "تغییر کرده"
-    : publish_status === 50
-    ? "دارای نقص"
-    : publish_status === 70
-    ? "در حال بررسی"
-    : publish_status === 100
-    ? "منتشر شده"
-    : "";
-
-export const ChapterWriteStatusDescription = (publish_status: number) =>
+export const writeStatusDescription = (publish_status: number) =>
   publish_status === 0
     ? "ذخیره شده"
     : publish_status === 10
