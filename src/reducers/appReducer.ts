75f026265a6f83d6cb6ee9ad6418429a46ef2b8e
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-08 23:02:53 +0430

    add message action

--- src/reducers/appReducer.ts
+++ src/reducers/appReducer.ts
@@ -6,12 +6,23 @@ const AppReducer = (state: IAppState = appInitialState, action: IAppAction) => {
   switch (action.type) {
     case AppActionType.CLOSE_SNACKBAR:
       return { ...state, snackbar: { ...state.snackbar, open: false } };
+
     case AppActionType.START_LOADING:
       return { ...state, loading: true };
 
     case AppActionType.STOP_LOADING:
       return { ...state, loading: false };
 
+    case AppActionType.STOP_LOADING_AND_MESSAGE:
+      return {
+        ...state,
+        loading: false,
+        snackbar: {
+          ...state.snackbar,
+          open: false,
+          message: action.payload?.msg,
+        },
+      };
     default:
       return state;
   }
