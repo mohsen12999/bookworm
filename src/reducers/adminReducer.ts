177ba544133b31e5514cb7f19535c17c542363ef
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-08 23:03:13 +0430

    add only admin action list

--- src/reducers/adminReducer.ts
+++ src/reducers/adminReducer.ts
@@ -14,6 +14,51 @@ const AdminReducer: any = (
         lastChapterId: action.payload?.chapter_id,
       };
 
+    case AdminActionType.SUCCESS_LOGIN:
+      return state;
+    case AdminActionType.FAILED_LOGIN:
+      return state;
+
+    case AdminActionType.SUCCESS_REGISTER:
+      return state;
+    case AdminActionType.FAILED_REGISTER:
+      return state;
+
+    case AdminActionType.SUCCESS_UPDATE_PROFILE:
+      return state;
+    case AdminActionType.FAILED_UPDATE_PROFILE:
+      return state;
+
+    case AdminActionType.SUCCESS_SAVING_BOOK:
+      return state;
+    case AdminActionType.FAILED_SAVING_BOOK:
+      return state;
+
+    case AdminActionType.SUCCESS_SAVING_POST:
+      return state;
+    case AdminActionType.FAILED_SAVING_POST:
+      return state;
+
+    case AdminActionType.SUCCESS_SAVING_CHAPTER:
+      return state;
+    case AdminActionType.FAILED_SAVING_CHAPTER:
+      return state;
+
+    case AdminActionType.SUCCESS_DELETING_BOOK:
+      return state;
+    case AdminActionType.FAILED_DELETING_BOOK:
+      return state;
+
+    case AdminActionType.SUCCESS_DELETING_POST:
+      return state;
+    case AdminActionType.FAILED_DELETING_POST:
+      return state;
+
+    case AdminActionType.SUCCESS_DELETING_CHAPTER:
+      return state;
+    case AdminActionType.FAILED_DELETING_CHAPTER:
+      return state;
+
     default:
       return state;
   }
