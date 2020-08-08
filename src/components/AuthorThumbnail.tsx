b48b77ffcbaeb8eb597e5f32c1bd1fa82e411c88
mohsen shabanian - work <mohsen1299@gmail.com>
2020-08-08 23:00:54 +0430

    add link to author thumb

--- src/components/AuthorThumbnail.tsx
+++ src/components/AuthorThumbnail.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { PublicPages } from "../constants/pages";
+import "./BookThumbnail.css";
 
 // TODO: new Author banner
 // TODO: best Author
@@ -12,10 +16,12 @@ interface IAuthorThumbnailProps {
 }
 
 const AuthorThumbnail = (props: IAuthorThumbnailProps) => (
-  <div className="author-thumb">
-    <img src={props.img} alt={props.name} />
-    <p>{props.name}</p>
-  </div>
+  <Link className="post-thumb" to={"/" + PublicPages.BOOKS + "/" + props.name}>
+    <div className="author-thumb">
+      <img src={props.img} alt={props.name} />
+      <p>{props.name}</p>
+    </div>
+  </Link>
 );
 
 export default AuthorThumbnail;
